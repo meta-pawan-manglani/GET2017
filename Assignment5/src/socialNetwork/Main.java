7fd0eee97d620fb57c7f95be0663fa7c4d440571
meta-pawan-manglani <pawan.manglani@metacube.com>
2017-07-31 15:43:37 +0530

    Update Main.java
    Now It will Print the record on Console

--- Assignment5/src/socialNetwork/Main.java
+++ Assignment5/src/socialNetwork/Main.java
@@ -15,17 +15,35 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		SocialNetwork obj = new SocialNetwork();
+		
+		/**
+		 * Printing The record on console
+		 */
+		System.out.println("******************Person Record******************");
+		System.out.println("Name"+"\t\t"+"EmailID"+"\t\t\t\t"+"Contact Number"+"\t\t"+"Native City");
+		
 		/**
 		 * Read from person List and add it to exist Node*
 		 */
 		for (Person p : SocialNetwork.personSet) {
 			obj.addNode(p);
+			p.getRecord();
 		}
+		
+		/**
+		 * Printing The record on console
+		 */
+		System.out.println();
+		System.out.println();
+		System.out.println("**************Organization Record******************");
+		System.out.println("Name"+"\t\t"+"EmailID"+"\t\t\t"+"Contact Number"+"\t\t"+"Type Of Company");
+		
 		/**
 		 * Read From Organization List and add it to existNode*
 		 */
 		for (Organization o : SocialNetwork.organizationSet) {
 			obj.addNode(o);
+			o.getRecord();
 		}
 
 		/**
