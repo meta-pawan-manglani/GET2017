d27e14e1df738ec78ee8482301928dea31a618dd
meta-pawan-manglani <pawan.manglani@metacube.com>
2017-08-21 16:21:52 +0530

    Update FileDao.java

--- RestfulWebService/src/com/metacube/DAO/FileDao.java
+++ RestfulWebService/src/com/metacube/DAO/FileDao.java
@@ -8,8 +8,6 @@ package com.metacube.DAO;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
