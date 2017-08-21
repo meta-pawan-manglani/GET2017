67cf15359c89f3277718823725d6b3c094b5a6fd
meta-pawan-manglani <pawan.manglani@metacube.com>
2017-08-21 16:23:55 +0530

    Update EmployeeFacade.java

--- RestfulWebService/src/com/metacube/Facade/EmployeeFacade.java
+++ RestfulWebService/src/com/metacube/Facade/EmployeeFacade.java
@@ -16,25 +16,25 @@ import com.metacube.io.WriteToFile;
  * The Class EmployeeFacade.
  * 
  * @author Pawan Manglani
- * @version 1.1 14-Aug-2017
+ * @version 1.1 18-Aug-2017
  */
 public class EmployeeFacade{
 
 	/** The employee map. binds the object of employee class to Employee id*/
 	private Map<String,Employee> employeeMap ;
-	
+
 	/** The employee. object of Employee class */
 	private Employee employee;
-	
+
 	/** The employee details. contains the list of all products */
 	private List<Employee> employeeDetails;
 
-	
+
 	/** The pf.  reference of EmployeeFacade*/
 	private static EmployeeFacade pf;
-	
-	
-        
+
+
+
 	/**
 	 * Instantiates a new employee facade.
 	 */
@@ -50,82 +50,88 @@ public class EmployeeFacade{
 	 */
 	public static EmployeeFacade getInstance(){
 		/**Double locking mechanism to make a singleton object**/
-		 if(pf==null){
-	       synchronized(EmployeeFacade.class){
-	    	   if(pf==null){
-	    		   pf = new EmployeeFacade();
-	    	   }
-	       }
-		 }
+		if(pf==null){
+			synchronized(EmployeeFacade.class){
+				if(pf==null){
+					pf = new EmployeeFacade();
+				}
+			}
+		}
 		return pf;
 	}
-	
+
 	/**
 	 * Gets the all employee.
-	 *
+	 * @return A string which contains status of the operation
 	 */
-	public List<Employee> getAllEmployee() throws IllegalArgumentException{
+	public String getAllEmployee() throws IllegalArgumentException{
+		
+		String result = Status.Success.toString();
+		try{
 		/**Make a DAO object**/
 		BaseDao bd = DAOFactory.getInstance(DBType.FileDao);
 		/**call the get products method specified in DAO**/
-               Status result;
 		employeeDetails = bd.getAllEmployee();
-                if(employeeDetails.size()==0){
-                    result = Status.Error_Occured;
-                }
-		/**Iterator to iterate the employee list**/
-		Iterator<Employee> itr = employeeDetails.iterator();
-		while(itr.hasNext()){
+		if(employeeDetails.size()==0){
+			result = Status.Error_Occured.toString() + " While Reading The file Database";
+		}
+		else{
+		 /**Iterator to iterate the employee list**/
+		 Iterator<Employee> itr = employeeDetails.iterator();
+		 while(itr.hasNext()){
 			employee = itr.next();
 			employeeMap.put(employee.getId(),employee);
+		 }
 		}
-                
-                
-                return employeeDetails;
+
+		}catch(Exception e){
+			result = Status.Error_Occured.toString();
+		}
+
+		return result;
 	}
-        
-        /*
-         *This method is used to print the products
-         *
-         *@return String array containing all the products
-         */
-        public String[] printEmployee(){
-            /**Iterator to iterate the employee list**/
-            String sar[] = new String[employeeDetails.size()+1];
-            int index = 1;
+
+	/*
+	 *This method is used to print the employees
+	 *
+	 *@return String array containing all the products
+	 */
+	public String[] printEmployee(){
+		/**Iterator to iterate the employee list**/
+		String sar[] = new String[employeeDetails.size()+1];
+		int index = 1;
 		Iterator<Employee> itr = employeeDetails.iterator();
-                sar[0] = "Employee Id\tEmployee Name\tEmployee Age";
-                while(itr.hasNext()){
-                    employee = itr.next();
-                    sar[index] = employee.toString();
-                    index++;
-                }
-               return sar;
-        }
+		sar[0] = "Employee Id\tEmployee Name\tEmployee Age";
+		while(itr.hasNext()){
+			employee = itr.next();
+			sar[index] = employee.toString();
+			index++;
+		}
+		return sar;
+	}
 	/**
-	 * Searchproduct.
+	 * SearchEmployee.
 	 *
 	 *This method search employee by using it's Id
 	 * @param id the id
-	 * @return Found if successful else NOT_Found
+	 * @return employee object
 	 */
 	public Employee searchEmployee(String id) {
-            BaseDao dao =  DAOFactory.getInstance(DBType.FileDao);
-            employee = (Employee)dao.searchEmployee(id);
+		BaseDao dao =  DAOFactory.getInstance(DBType.FileDao);
+		employee = (Employee)dao.searchEmployee(id);
 		return employee;
-		
 	}
-	
+
 	/**
-	 * This function writes  into the file
+	 * This function writes  into the file when a new employee is added
 	 * @param emp the employee id
 	 */
 	public void writeFile(Employee emp){
 		WriteToFile.writeFile(emp);
 	}
-	
+
 	/**
-	 * This function writes  into the file
+	 * This function writes  into the file when a employee is deleted
 	 * @param employeeMap the map containing the employee object
 	 */
 	public void writeFile(Map<String,Employee> employeeMap){
@@ -138,15 +144,19 @@ public class EmployeeFacade{
 	 */
 	public Status addEmployee(Employee emp){
 		Status answer;
+		try{
 		if(employeeMap.containsKey(emp.getId())){
 			answer = Status.Duplicate;
 		}
 		else{
-		employeeMap.put(emp.getId(),emp);
-		employeeDetails.add(emp);
-		writeFile(emp);
-		answer = Status.Success;
-	    }
+			employeeMap.put(emp.getId(),emp);
+			employeeDetails.add(emp);
+			writeFile(emp);
+			answer = Status.Success;
+		}
+		}catch(Exception e){
+			answer = Status.Error_Occured;
+		}
 		return answer;
 	}
 	/*
@@ -155,14 +165,22 @@ public class EmployeeFacade{
 	 * @return Status return the status of operation
 	 */
 	public Status deleteEmployee(String id){
+		
 		Status answer;
+		try{
+		id = id.trim();
 		if(employeeMap.containsKey(id)){
+			employeeDetails.remove(employeeMap.get(id));
 			employeeMap.remove(id);
+			System.out.println(employeeMap.toString());
 			writeFile(employeeMap);
 			answer = Status.Success;
 		}else{
 			answer = Status.NOT_Found;
 		}
+	  }catch(Exception e){
+		  answer = Status.Error_Occured;
+	  }
 		return answer;
 	}
 
