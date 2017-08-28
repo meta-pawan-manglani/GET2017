5d124f7816a3402f3205a884dbc2592651440444
meta-pawan-manglani <pawan.manglani@metacube.com>
2017-08-28 15:35:03 +0530

    Controller.java

--- src/com/metacube/roomAllotment/controller/Controller.java
+++ src/com/metacube/roomAllotment/controller/Controller.java
@@ -9,48 +9,58 @@ import com.metacube.roomAllotment.facade.AllotmentFacade;
  * @version 1.1 25-Aug-2017
  */
 public class Controller {
-	
+
 	/** The af. */
 	private AllotmentFacade af; 
-	
+
 	/**
 	 * Instantiates a new controller.
 	 */
 	public Controller() {
-		 af = AllotmentFacade.getInstance();
+		af = AllotmentFacade.getInstance();
+	}
+
+
+
+	/**
+	 * Free rooms.
+	 *
+	 * @return the int
+	 */
+	public int freeRooms() {
+		int num = af.freeRooms();
+		return num;
+	} 
+
+	/**
+	 * Allot room.
+	 *
+	 * @param guest the guest
+	 * @return the int
+	 */
+	public int allotRoom(Guest guest) {
+		return af.allotRooms(guest);
 	}
 	
-
-
-	 /**
- 	 * Free rooms.
- 	 *
- 	 * @return the int
- 	 */
- 	public int freeRooms() {
-		  int num = af.freeRooms();
-		  return num;
-	  } 
-	 
-	 /**
- 	 * Allot room.
- 	 *
- 	 * @param guest the guest
- 	 * @return the int
- 	 */
- 	public int allotRoom(Guest guest) {
-		 return af.allotRooms(guest);
-	 }
-        
-        public String getAllGuest(){
-            String answer= af.getAllGuest();
-            return answer;
-        }
-        
-        public Guest getGuest(String name,int age){
-            Guest guest = new Guest();
-            guest.addDetails(name, age);
-            return af.getGuest(guest);
-        }
+	
+	/**
+	 * This method gives information of all guest
+	 * @return
+	 */
+	public String getAllGuest(){
+		String answer= af.getAllGuest();
+		return answer;
+	}
+	
+	/**
+	 * This method search for particular guest entry
+	 * @return
+	 */
+	public Guest getGuest(String name,int age){
+		Guest guest = new Guest();
+		name = name.trim().toUpperCase();
+		guest.addDetails(name, age);
+		return af.getGuest(guest);
+	}
 
 }
