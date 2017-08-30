6814190e282d267646757ad8c4cc5a823ded7640
meta-pawan-manglani <pawan.manglani@metacube.com>
2017-08-30 15:02:15 +0530

    Update Controller.java

--- src/com/metacube/roomAllotment/controller/Controller.java
+++ src/com/metacube/roomAllotment/controller/Controller.java
@@ -28,7 +28,12 @@ public class Controller {
 	 * @return the int
 	 */
 	public int freeRooms() {
-		int num = af.freeRooms();
+		int num = 0;
+		try {
+			num = af.freeRooms(); 
+		}catch(Exception e) {
+			System.out.println("error in calculating free rooms" + e.getMessage());
+		}
 		return num;
 	} 
 
@@ -38,29 +43,39 @@ public class Controller {
 	 * @param guest the guest
 	 * @return the int
 	 */
-	public int allotRoom(Guest guest) {
-		return af.allotRooms(guest);
+	public void allotRoom(Guest guest) {
+		af.allotRooms(guest);
 	}
-	
-	
+
+
 	/**
 	 * This method gives information of all guest
 	 * @return
 	 */
 	public String getAllGuest(){
-		String answer= af.getAllGuest();
+		String answer = "";
+		try {
+			answer= af.getAllGuest();
+		}catch(Exception e) {
+			System.out.println("Error while getting all guest information "+ e.getMessage());
+		}
 		return answer;
 	}
-	
+
 	/**
 	 * This method search for particular guest entry
 	 * @return
 	 */
 	public Guest getGuest(String name,int age){
 		Guest guest = new Guest();
-		name = name.trim().toUpperCase();
-		guest.addDetails(name, age);
-		return af.getGuest(guest);
+		try {
+			name = name.trim().toUpperCase();
+			guest.addDetails(name, age);
+			guest =  af.getGuest(guest);
+		}catch(Exception e) {
+			System.out.println("Error while getting guest information "+ e.getMessage());
+		}
+		return guest;
 	}
 
 }
