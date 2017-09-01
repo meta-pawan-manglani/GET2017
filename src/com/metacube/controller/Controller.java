bfcde00eea54a38f8f45557011e373de7de8bba4
meta-pawan-manglani <pawan.manglani@metacube.com>
2017-09-01 10:18:14 +0530

    Ds Session 7

--- src/com/metacube/controller/Controller.java
+++ src/com/metacube/controller/Controller.java
@@ -25,33 +25,37 @@ public class Controller {
 		Tree<Integer> tree = new Tree<>();
 		/*helper variable*/
 		int choice = 0,element = 0;
-		while(true) {
-			/*printing menu*/
-			Input.printMenu();
-			/*taking user choice*/
-			choice = Input.takeIntegerAsInput("Choice");
-			switch(choice) {
-			/*Case to insert an element in tree*/
-			case 1:
-				element = Input.takeIntegerAsInput("Element to insert");
-				tree.insertData(element);
-				break;
-				/*Case to print tree*/	
-			case 2:
-				Input.print(tree.toString());
-				break;
-				/*case to search an element in tree*/
-			case 3:
-				element = Input.takeIntegerAsInput("Element to search");
-				Input.print(Integer.toString(tree.search(element)));
-				break;
-				/*case to exit*/
-			case 4:
-				Input.close();
-				System.exit(0);
-			default :
-				Input.print("Please Select a valid menu");
+		try {
+			while(true) {
+				/*printing menu*/
+				Input.printMenu();
+				/*taking user choice*/
+				choice = Input.takeIntegerAsInput("Choice");
+				switch(choice) {
+				/*Case to insert an element in tree*/
+				case 1:
+					element = Input.takeIntegerAsInput("Element to insert");
+					tree.insertData(element);
+					break;
+					/*Case to print tree*/	
+				case 2:
+					Input.print(tree.toString());
+					break;
+					/*case to search an element in tree*/
+				case 3:
+					element = Input.takeIntegerAsInput("Element to search");
+					Input.print(Integer.toString(tree.search(element)));
+					break;
+					/*case to exit*/
+				case 4:
+					Input.close();
+					System.exit(0);
+				default :
+					Input.print("Please Select a valid menu");
+				}
 			}
+		}catch(Exception e) {
+			Input.print(e.getMessage());
 		}
 
 	}
