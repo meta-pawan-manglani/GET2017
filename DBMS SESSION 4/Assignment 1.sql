123819eb9eaa0427c8bc06524b631ffab768bf77
pManglani <manglanipawan5@gmail.com>
2017-09-08 14:37:11 +0530

    DBMS-SESSION-4

---
+++ DBMS SESSION 4/Assignment 1.sql
@@ -0,0 +1,47 @@
+/******************************
+** File: DBMS Session4 Assignment1  
+** Auth: Pawan Manglani
+** Date: 7-Aug-2017
+******************************/
+
+/*use database lis*/
+USE lis;
+
+/*This query will display members of a particular category*/
+SELECT member_nm FROM members 
+/*Filter condition that filter the members on the basis of there category*/
+WHERE category = (SELECT category 
+                    FROM members WHERE member_nm = 'pawan13');
+
+/*This query will display the information of issued books*/
+SELECT i.issue_dt,i.due_dt,mb.member_nm,t.title_nm FROM book_issue bi 
+/*joining book_return table and book_issue table*/
+JOIN book_return r ON r.return_dt IS NULL AND bi.accession_no = r.accession_no AND bi.member_id = r.member_id,
+books b 
+/*joining titles table and books table*/
+JOIN titles t ON b.title_id = t.title_id,
+members mb 
+/*filter condition it takes the data from book_return table*/
+WHERE mb.member_id IN (SELECT r.member_id FROM book_return) AND b.accession_no IN (SELECT r.accession_no FROM book_return) ; 
+
+/* This query will display the information of books which are not returned on time*/
+SELECT i.issue_dt,i.due_dt,r.return_dt,mb.member_nm,t.title_nm
+FROM book_issue i JOIN book_return r ON i.accession_no = r.accession_no AND i.member_id = r.member_id,
+books b JOIN titles t ON b.title_id = t.title_id,
+members mb 
+WHERE DATEDIFF(r.return_dt,i.due_dt) >=1 AND mb.member_id IN (SELECT r.member_id FROM book_return) AND b.accession_no IN (SELECT r.accession_no FROM book_return) ;
+
+/* This query will display the title of books which has price equivalent to highest price*/
+SELECT b.title_id,t.title_nm,b.price,b.accession_no FROM books b
+/*joining table books and title*/
+JOIN titles t ON b.title_id = t.title_id
+/*filter condition gives the name of books where price is highest among all the books*/
+WHERE b.price = ( SELECT MAX(price) FROM books WHERE purchase_dt IS NOT NULL);
+
+/* This query will display book which have second highest price*/
+SELECT MAX(price)AS SecondMaximum,title_id FROM books
+/*filter condition which select the price which has price lower than highest price*/
+WHERE price < (SELECT MAX(price) FROM BOOKS);
+
+
+describe books
---
+++ DBMS SESSION 4/Assignment 2.sql
@@ -0,0 +1,54 @@
+/******************************
+** File: DBMS Session4 Assignment2  
+** Auth: Pawan Manglani
+** Date: 7-Aug-2017
+******************************/
+
+/*use database lis*/
+USE lis;
+
+/*This command will drop view of issued books if exists*/
+DROP VIEW IF EXISTS IssuedBooks;
+/*This command will create view for details of issued books*/
+CREATE VIEW IssuedBooks
+AS
+/*making of view*/
+SELECT mb.member_nm,i.member_id,i.accession_no,i.issue_dt,i.due_dt FROM book_issue i
+/*joining table books and memebers*/
+JOIN members mb ON i.member_id = mb.member_id;
+/*This command will display the view issuedBooks*/
+SELECT member_nm,member_id,accession_no,issue_dt,due_dt
+FROM IssuedBooks;
+
+/*This command will drop view of MemberDetails if exists*/
+DROP VIEW IF EXISTS MemberDetails;
+/*This command will create view for details of members of a particular category*/
+CREATE VIEW MemberDetails
+AS
+/*making of view*/
+SELECT member_nm,member_id,category
+FROM members;
+/*This query will display the view members*/
+SELECT member_nm,member_id,IF(category='S','Student',IF(category='F','Faculty','Others')) AS Category
+FROM MemberDetails;
+
+/*This command will drop view of BookDetails if exists*/
+DROP VIEW IF EXISTS BookDetails;
+/*This command will create view for book details*/
+CREATE VIEW BookDetails
+AS 
+/*making of view selecting column which has to be displayed in view*/
+SELECT mb.member_nm,s.subject_nm,t.title_nm,mb.category,i.issue_dt,i.due_dt,r.return_dt FROM book_issue i 
+/*joining table book_return and book_issue*/
+JOIN book_return r ON i.accession_no = r.accession_no AND i.member_id = r.member_id
+/*joining table book_issue and books*/
+JOIN books b ON i.accession_no = b.accession_no
+/*joining table titles and books*/
+JOIN titles t ON b.title_id = t.title_id 
+/*joining table subjects and titles*/
+JOIN subjects s ON t.subject_id = s.subject_id,
+members mb
+WHERE mb.member_id = i.member_id;
+/*This query will display the view BookDetails*/
+SELECT member_nm,subject_nm,title_nm,category,issue_dt,due_dt,return_dt
+FROM BookDetails;
