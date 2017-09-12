bfe460e4bdd5e752314fcea81aa7ec29e7c49466
meta-pawan-manglani <pawan.manglani@metacube.com>
2017-09-12 18:48:49 +0530

    DBMS-SESSION-4

--- DBMS SESSION 4/Assignment 1.sql
+++ DBMS SESSION 4/Assignment 1.sql
@@ -1,7 +1,7 @@
 /******************************
 ** File: DBMS Session4 Assignment1  
 ** Auth: Pawan Manglani
-** Date: 7-Aug-2017
+** dt: 7-Aug-2017
 ******************************/
 
 /*use database lis*/
@@ -13,28 +13,53 @@ SELECT member_nm FROM members
 WHERE category = (SELECT category 
                     FROM members WHERE member_nm = 'pawan13');
 
-/*This query will display the information of issued books*/
-SELECT bi.issue_dt,bi.due_dt,mb.member_nm,t.title_nm FROM book_issue bi 
-/*joining book_return table and book_issue table*/
-JOIN book_return r ON r.return_dt IS NULL AND bi.accession_no = r.accession_no AND bi.member_id = r.member_id,
-books b 
-/*joining titles table and books table*/
-JOIN titles t ON b.title_id = t.title_id,
-members mb 
-/*filter condition it takes the data from book_return table*/
-WHERE EXISTS (SELECT mb.member_nm FROM members WHERE r.member_id = mb.member_id );  
-
 /* This query will display the information of books which are not returned on time*/
-SELECT i.issue_dt,i.due_dt,r.return_dt,mb.member_nm,t.title_nm
-FROM book_issue i
-/*joining table book_issue and book_return*/
-JOIN book_return r ON i.accession_no = r.accession_no AND i.member_id = r.member_id,
+SELECT issue_dt,titles.title_nm AS 'Title',members.member_nm AS 'Member name', due_dt AS 'Due dt'
+FROM book_issue bi 
+/*joining members table and book_issue table*/
+JOIN members ON bi.member_id = members.member_id
+/*joining titles table and books table*/
+JOIN titles ON titles.title_id = (SELECT title_id FROM books WHERE bi.accession_no = books.accession_no)
+/*filter condition it takes the data from book_return table*/
+WHERE EXISTS (SELECT br.accession_no, br.member_id 
+                      FROM book_return br
+                      WHERE br.member_id = bi.member_id AND br.accession_no = bi.accession_no and br.return_dt Is NULL);  
+
+/*Case 2 when some entries are not in book return table*/
+SELECT bi.issue_dt AS 'Issue date',titles.title_nm AS 'Title',members.member_nm AS 'Member name', due_dt  AS 'Due date'
+FROM book_issue bi 
+/*joining table members and book_issue*/
+LEFT JOIN members ON bi.member_id = members.member_id
 /*joining table books and titles*/
-books b JOIN titles t ON b.title_id = t.title_id,
-members mb 
-WHERE DATEDIFF(r.return_dt,i.due_dt) >=1 AND  EXISTS (SELECT mb.member_nm FROM members WHERE r.member_id = mb.member_id );
+LEFT JOIN titles ON titles.title_id = (SELECT books.title_id FROM books WHERE books.accession_no = bi.accession_no)
+/*filter condition it takes the data from book_return table*/
+ WHERE EXISTS (SELECT br.accession_no, br.member_id 
+                   FROM book_return br 
+                   WHERE DATEDIFF(br.return_dt,bi.due_dt) >=1 AND bi.member_id = br.member_id AND bi.accession_no = br.accession_no); 
+   
+/*case 3 when a entry is done in book return table only when book is returned*/
+   SELECT issue_date,titles.name AS 'Title',members.name AS 'Member name', due_date AS 'Due date'
+   FROM book_issue bi 
+   JOIN members ON bi.member_id = members.id
+   JOIN titles ON titles.id = (SELECT title_id FROM books WHERE bi.accession_no = books.accession_no) 
+   WHERE NOT EXISTS (SELECT br.accession_no, br.member_id 
+                       FROM book_return br
+                       WHERE br.member_id = bi.member_id AND br.accession_no = bi.accession_no); 
 
-
+/*This query will display the information of books which are returned after due date*/
+SELECT bi.issue_dt AS 'Issue date',titles.title_nm AS 'Title',members.member_nm AS 'Member name', due_dt  AS 'Due date'
+FROM book_issue bi 
+/*joining table members and book_issue*/
+JOIN members ON bi.member_id = members.member_id
+/*joining table books and titles*/
+JOIN titles ON titles.title_id = (SELECT books.title_id FROM books WHERE books.accession_no = bi.accession_no)
+/*filter condition it takes the data from book_return table*/
+ WHERE EXISTS (SELECT br.accession_no, br.member_id 
+                   FROM book_return br 
+                   WHERE DATEDIFF(br.return_dt,bi.due_dt) >=1 AND bi.member_id = br.member_id AND bi.accession_no = br.accession_no); 
+                    
+                       
+                       
 /* This query will display the title of books which has price equivalent to highest price*/
 SELECT b.title_id,t.title_nm,b.price,b.accession_no FROM books b
 /*joining table books and title*/
@@ -45,7 +70,4 @@ WHERE b.price = ( SELECT MAX(price) FROM books WHERE purchase_dt IS NOT NULL);
 /* This query will display book which have second highest price*/
 SELECT MAX(price)AS SecondMaximum,title_id FROM books
 /*filter condition which select the price which has price lower than highest price*/
-WHERE price < (SELECT MAX(price) FROM BOOKS);
-
-
-describe books
+WHERE price < (SELECT MAX(price) FROM BOOKS); 
